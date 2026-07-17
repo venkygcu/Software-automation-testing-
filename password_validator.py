4dc5d5283dfd7614b417172afe5f9c584aa313ae
Gunji Venkatesh <166098987+venkygcu@users.noreply.github.com>
2026-07-17 07:33:27 +0530

    Delete password_validator.py

--- password_validator.py
+++
@@ -1,9 +0,0 @@
-def is_valid_password(password):
-    return 18 <= len(password) <= 60
-
-
-password = input("Enter your password: ")
-if is_valid_password(password):
-    print("Valid password")
-else:
-    print("Invalid password. Length must be between 18 and 60 characters")
